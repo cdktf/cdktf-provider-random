64f3b5cc6e5be80b0c7cefeb10d056060e98dd43
Ansgar Mertens <ansgar@hashicorp.com>
2022-01-07 11:10:21 +0100

    fix: add `jsiiVersion` to `.projenrc.js`

--- .projenrc.js
+++ .projenrc.js
@@ -4,6 +4,7 @@ const project = new CdktfProviderProject({
   cdktfVersion: "^0.8",
   constructsVersion: "^10.0.25",
   minNodeVersion: "14.17.0",
+  jsiiVersion: "^1.46.0",
 });
 
 project.synth();
