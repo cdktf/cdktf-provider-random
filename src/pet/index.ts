d9d2bb792e5f1136058e3b4bda9fa4979d698f8f
github-team-tf-cdk <github-team-tf-cdk@hashicorp.com>
2024-01-11 13:24:36 +0000

    chore(deps): update  by cdktf/cdktf-repository-manager@af3609a

--- src/pet/index.ts
+++ src/pet/index.ts
@@ -1,8 +1,3 @@
-/**
- * Copyright (c) HashiCorp, Inc.
- * SPDX-License-Identifier: MPL-2.0
- */
-
 // https://registry.terraform.io/providers/hashicorp/random/3.6.0/docs/resources/pet
 // generated from terraform resource schema
 
